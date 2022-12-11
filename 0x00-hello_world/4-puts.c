2d661ba3eb312c51ec802cc5391694698971e54c
natashakeyana <natashakeyana@gmail.com>
2022-12-11 04:04:00 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main-Entry
- * Return: Always 0
+ * main- Entry Point
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multi lingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
