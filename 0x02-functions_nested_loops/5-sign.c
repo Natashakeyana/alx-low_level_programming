df4b61ab36f12d61bd92150870871c52419ecec1
natashakeyana <natashakeyana@gmail.com>
2023-01-03 10:35:40 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,15 +1,27 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabets
- * @c: character to be checked
+ * print_sign - prints the sign of a number
  *
- * Return: 1 if character is a letter, 0 otherwise
+ * @n: number whose sign is to be printed
+ *
+ * Return: 1 if n > 0, 0 if n == 0 and -1 if n < 0
  */
-int _isalpha(int c)
+int print_sign(int n)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (n > 0)
+	{
+		_putchar('+');
 		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
 	else
+	{
+		_putchar(0 + '0');
 		return (0);
+	}
 }
